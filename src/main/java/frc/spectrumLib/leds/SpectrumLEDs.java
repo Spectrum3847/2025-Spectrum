8bdbf707518bfaec1a1b0947f62787eb868ed4d5
JediMasterLucy <lucia.hans@icloud.com>
2025-06-27 14:43:11 -0500

    fixed bugs

--- src/main/java/frc/spectrumLib/leds/SpectrumLEDs.java
+++ src/main/java/frc/spectrumLib/leds/SpectrumLEDs.java
@@ -419,41 +419,40 @@ public class SpectrumLEDs implements SpectrumSubsystem {
         };
     }
 
-    public LEDPattern rainbowBlink(double blink) {
+    // public LEDPattern rainbowBlink(double blink) {
 
+    //     return new LEDPattern() {
+    //         @Override
+    //         public void applyTo(LEDReader reader, LEDWriter writer) {
+    //             int bufLen = reader.getLength();
+    //             long currentTime = System.currentTimeMillis();
+    //             double cycleTime =
+    //                    durationInSeconds
+    //                             * 1000; // Convert time to milliseconds for the entire cycle
+    //              double phase =
+    //                     (currentTime % cycleTime) / cycleTime; // Phase of the cycle from 0 to 1
 
+    //             // Determine direction and position based on the phase
+    //             boolean backwards = phase > 0.5;
+    //             double position = backwards ? 2 * (1 - phase) : 2 * phase;
+    //             int ledPosition = (int) (position * bufLen);
 
-        return new LEDPattern() {
-            @Override
-            public void applyTo(LEDReader reader, LEDWriter writer) {
-                int bufLen = reader.getLength();
-                long currentTime = System.currentTimeMillis();
-                // double cycleTime =
-                        // durationInSeconds
-                                //* 1000; // Convert time to milliseconds for the entire cycle
-                // double phase =
-                //         (currentTime % cycleTime) / cycleTime; // Phase of the cycle from 0 to 1
-
-                // Determine direction and position based on the phase
-                boolean backwards = phase > 0.5;
-                double position = backwards ? 2 * (1 - phase) : 2 * phase;
-                int ledPosition = (int) (position * bufLen);
-
-                for (int led = 0; led < bufLen; led++) {
-                    if (led == ledPosition) {
-                        writer.setLED(led, c);
-                    } else if (led == ledPosition - 1 || led == ledPosition + 1) {
-                        writer.setLED(led, new Color(c.red * 0.66, c.green * 0.66, c.blue * 0.66));
-                    } else if (led == ledPosition - 2 || led == ledPosition + 2) {
-                        writer.setLED(led, new Color(c.red * 0.33, c.green * 0.33, c.blue * 0.33));
-                    } else {
-                        writer.setLED(led, Color.kBlack);
-                    }
-                }
-            };
-        };
-    }
-    } 
+    //            /*  for (int led = 0; led < bufLen; led++) {
+    //                 if (led == ledPosition) {
+    //                     writer.setLED(led, c);
+    //                 } else if (led == ledPosition - 1 || led == ledPosition + 1) {
+    //                     writer.setLED(led, new Color(c.red * 0.66, c.green * 0.66, c.blue *
+    // 0.66));
+    //                 } else if (led == ledPosition - 2 || led == ledPosition + 2) {
+    //                     writer.setLED(led, new Color(c.red * 0.33, c.green * 0.33, c.blue *
+    // 0.33));
+    //                 } else {
+    //                     writer.setLED(led, Color.kBlack);
+    //                 } */
+    //             }
+    //         };
+    //     };
+}
 
     // LEDPattern Methods
     // reversed()
@@ -469,4 +468,3 @@ public class SpectrumLEDs implements SpectrumSubsystem {
     // atBrightness(Dimensionless relativeBrightness)
     // progressMaskLayer(DoubleSupplier progressSupplier)
     // steps(Map<? extends Number, Color> steps)
-}
