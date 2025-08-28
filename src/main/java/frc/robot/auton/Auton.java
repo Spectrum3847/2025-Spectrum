46f0e5c4cef2e663327e8fcf236f130bcb7a1872
Spectrum3847 <spectrum3847@gmail.com>
2025-08-28 15:39:47 -0500

    ...

--- src/main/java/frc/robot/auton/Auton.java
+++ src/main/java/frc/robot/auton/Auton.java
@@ -130,10 +130,7 @@ public class Auton {
     public Command secretAuto(boolean mirrored) {
         return SpectrumAuton("SecretAuto", mirrored)
                 .withTimeout(2)
-                .andThen(
-                        aimL4score(),
-                        SpectrumAuton("SecretAuto", mirrored),
-                        aimL4score());
+                .andThen(aimL4score(), SpectrumAuton("SecretAuto", mirrored), aimL4score());
     }
 
     public Command testAuto(boolean mirrored) {
