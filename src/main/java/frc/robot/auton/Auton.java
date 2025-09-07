705bb7eac6663ec194beb8c3b12e012f787bfd3e
JediMasterLucy <lucia.hans@icloud.com>
2025-09-06 22:48:26 -0500

    socks

--- src/main/java/frc/robot/auton/Auton.java
+++ src/main/java/frc/robot/auton/Auton.java
@@ -82,8 +82,8 @@ public class Auton {
         pathChooser.addOption("roiSecretAutoTest", secretAutoTest(false));
         pathChooser.addOption("lefSecretAutoTest", secretAutoTest(true));
 
-        pathChooser.addOption("roiSockretAuto", sockretAuto(false));
-        pathChooser.addOption("lefSockretAuto", sockretAuto(true));
+        // pathChooser.addOption("roiSockretAuto", sockretAuto(false));
+        // pathChooser.addOption("lefSockretAuto", sockretAuto(true));
 
         // pathChooser.addOption("roiSecretAuto2", secretAuto2(false));
         // pathChooser.addOption("lefSecretAuto2", secretAuto2(true));
@@ -168,14 +168,14 @@ public class Auton {
                 .withName("TEST FULL");
     }
 
-    public Command sockretAuto(boolean mirrored) {
-        return (SpectrumAuton("SpectrumAuto0.5", mirrored)
-                        .andThen(
-                                fullSequenceAimL4Score(1),
-                                SpectrumAuton("SockretAuto", mirrored),
-                                fullSequenceAimL4Score(1)))
-                .withName("Sockret Auto");
-    }
+    // public Command sockretAuto(boolean mirrored) {
+    //     return (SpectrumAuton("SecretAuto0.5", mirrored)
+    //                     .andThen(
+    //                             fullSequenceAimL4Score(1),
+    //                             SpectrumAuton("SockretAuto", mirrored),
+    //                             fullSequenceAimL4Score(1)))
+    //             .withName("Sockret Auto");
+    // }
 
     // Aaron's code (To see the difference between ours and his)
     public Command secretAutoTest2(boolean mirrored) {
