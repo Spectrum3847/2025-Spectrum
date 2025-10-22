8daf6d6300d10f406af718e4c5135b42ce881911
Spectrum3847 <spectrum3847@gmail.com>
2025-10-22 18:41:54 -0500

    starting to work on subsystem states

---
+++ src/main/java/frc/robot/intake/IntakeState.java
@@ -0,0 +1,10 @@
+package frc.robot.intake;
+
+public enum IntakeState {
+    NEUTRAL,
+    CORAL_HOLD,
+    ALGAE_HOLD,
+    CORAL_SCORE,
+    ALGEA_SCORE,
+    EJECT_MODE,
+}
---
+++ src/main/java/frc/robot/twist/TwistState.java
@@ -0,0 +1,11 @@
+package frc.robot.twist;
+
+public enum TwistState {
+    HOME,
+    LEFT_CORAL_SCORE,
+    RIGHT_CORAL_SCORE,
+    ALGAE_INTAKE,
+    GROUND_INTAKE,
+    NET_ALGAE_TURRET,
+    NET_ALGAE_FIXED
+}
